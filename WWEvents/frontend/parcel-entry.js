df6e92e0837cd3b0b2ffb6b58cb8a249113fa8dd
Maximilian Gutwein <maximilian@gutwe.in>
2022-05-28 14:31:19 +0200

    Update deps

--- WWEvents/frontend/parcel-entry.js
+++ WWEvents/frontend/parcel-entry.js
@@ -1,4 +1,4 @@
-import '../../node_modules/fomantic-ui-css/semantic.css'
+import '../../node_modules/fomantic-ui/dist/semantic.css'
 import '../../node_modules/chart.js/dist/Chart.css'
 import '../../node_modules/jquery-datetimepicker/jquery.datetimepicker.css'
 import './main.css';
@@ -8,4 +8,4 @@ window.$ = window.jQuery = jQuery;
 
 require("jquery-datetimepicker")
 require("chart.js")
-require("fomantic-ui-css/semantic")
+require("fomantic-ui/dist/semantic")
